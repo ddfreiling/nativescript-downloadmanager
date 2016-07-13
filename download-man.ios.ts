7f9b5b61c104b268f38b83050e72e5c87bb09255
Daniel Dam Freiling <dfg@Nota.dk>
2016-07-13 12:59:32 +0200

    Add NSURLSessionDelegate implementation

--- download-man.ios.ts
+++ download-man.ios.ts
@@ -13,4 +13,33 @@ export class DownloadManager extends Common {
   cancelDownloads(...refIds: number[]): void {}
   cancelAllDownloads(): void {}
   destroy(): void {}
+
+class TNSSessionDelegate extends NSObject implements NSURLSessionDelegate, NSURLSessionTaskDelegate {
+  protocols: ['NSURLSessionDelegate', 'NSURLSessionTaskDelegate'];
+
+  URLSessionDidBecomeInvalidWithError(session: NSURLSession, error: NSError): void {
+    console.log(`DownloadManager.iOS - Session did become invalid callback`);
+  }
+	URLSessionDidReceiveChallengeCompletionHandler(session: NSURLSession, challenge: NSURLAuthenticationChallenge, completionHandler: (arg1: number, arg2: NSURLCredential) => void): void {
+    console.log(`DownloadManager.iOS - Received challenge completion callback`);
+  }
+	URLSessionDidFinishEventsForBackgroundURLSession(session: NSURLSession): void {
+    console.log(`DownloadManager.iOS - Finished events for background session`);
+  }
+
+  URLSessionTaskWillPerformHTTPRedirectionNewRequestCompletionHandler(session: NSURLSession, task: NSURLSessionTask, response: NSHTTPURLResponse, request: NSURLRequest, completionHandler: (arg1: NSURLRequest) => void): void {
+    console.log(`DownloadManager.iOS - Will perform HTTP redirect`);
+  }
+	URLSessionTaskDidReceiveChallengeCompletionHandler(session: NSURLSession, task: NSURLSessionTask, challenge: NSURLAuthenticationChallenge, completionHandler: (arg1: number, arg2: NSURLCredential) => void): void {
+    console.log(`DownloadManager.iOS - Received challenge completed`);
+  }
+	URLSessionTaskNeedNewBodyStream(session: NSURLSession, task: NSURLSessionTask, completionHandler: (arg1: NSInputStream) => void): void {
+    console.log(`DownloadManager.iOS - New body stream`);
+  }
+	URLSessionTaskDidSendBodyDataTotalBytesSentTotalBytesExpectedToSend(session: NSURLSession, task: NSURLSessionTask, bytesSent: number, totalBytesSent: number, totalBytesExpectedToSend: number): void {
+    console.log(`DownloadManager.iOS - Bytes sent, bytes expected to send...`);
+  }
+	URLSessionTaskDidCompleteWithError(session: NSURLSession, task: NSURLSessionTask, error: NSError): void {
+    console.log(`DownloadManager.iOS - Did complete with error`);
+  }
 }
