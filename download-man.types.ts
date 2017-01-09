eca6d6ddea496f8a4397c0aec68a3fc7dba0a5c6
Daniel Freiling <dfg@nota.dk>
2017-01-09 17:16:02 +0100

    chore: improved documentation

--- download-man.types.ts
+++ download-man.types.ts
@@ -43,16 +43,54 @@ export interface DownloadStatus {
 }
 
 export declare class DownloadManager {
+  /**
+   * Downloads a single file using a DownloadRequest
+   * @return Promise<number> with reference ID to the started download
+   */
   downloadFile(request: DownloadRequest): Promise<number>;
+  /**
+   * Checks if a download is currently in-progress using a reference ID
+   */
   isDownloadInProgress(refId: number): boolean;
+  /**
+   * Checks if a given DownloadState is categorized as being in-progress
+   */
   isInProgress(state: DownloadState): boolean;
+  /**
+   * Gets the reference IDs of all downloads in progress
+   */
   getDownloadsInProgress(): number[];
+  /**
+   * Gets the DownloadState of a single download, for a given reference ID
+   */
   getDownloadState(refId: number): DownloadState;
+  /**
+   * Gets a detailed DownloadStatus for a given reference ID
+   */
   getDownloadStatus(refId: number): DownloadStatus;
+  /**
+   * Cancels downloads based on one or more reference IDs
+   */
   cancelDownloads(...refIds: number[]): void;
+  /**
+   * Cancels all active downloads
+   */
   cancelAllDownloads(): void;
+  /**
+   * Gets the absolute and canonical dir path,
+   * for storing files externally
+   */
   getExternalFilesDirPath(): string;
+  /**
+   * Gets the available disk space in bytes on the native platform
+   */
   getAvailableDiskSpaceInBytes(): number;
+  /**
+   * Gets the size of a local file in bytes
+   */
   getSizeOfFile(localFilePath: string): number;
+  /**
+   * Clean up any resources or subscriptions held
+   */
   destroy(): void;
 }
